e283241d3611c9c937fc8dbb8cf8bd0a5a750710
Patrick Mc Gonagle <patrickmcg2@gmail.com>
2020-09-03 21:01:39 +0100

    twitter class added

--- src/main/java/paddymcg/App.java
+++ src/main/java/paddymcg/App.java
@@ -1,5 +1,11 @@
 package paddymcg;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.BrowserType;
+
+import paddymcg.core.WebDriverHelper;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldo!" );
+        
+        WebDriverHelper.setDriverLocation("C:\\WebDrivers");
+
+        WebDriver driver = WebDriverHelper.open(BrowserType.CHROME);
+        driver.navigate().to("https://twitter.com");
+        try {
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
+        Twitter twitter = new Twitter(driver);
+        WebElement l = twitter.loginField();
+        WebElement p = twitter.pwdField();
+
+        l.sendKeys("qwwe");
+        // p.sendKeys("pamsdnjasn");
+
+        System.out.println(l);
+        System.out.println(p);
+
+        WebDriverHelper.close(driver, true);
+
     }
 }
